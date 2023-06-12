7225b577bb25a7fea2f00e06f9b34c520a0fefa1
Raí Silva <121699235+raaisilvaa@users.noreply.github.com>
2023-06-11 22:32:26 -0300

    .

---
+++ aula 6/index.js
@@ -0,0 +1,7 @@
+let nome = 'Raí'; // String = Texto
+
+console.log(nome, 'nasceu em 1993.');
+console.log('Em 2016', nome, 'conheceu Irene.');
+console.log(nome, 'descobriu em 2017 que Irene era sua mãe.');
+console.log('Irene contou do irmão para', nome, 'em 2018.');
+console.log('O nome do irmão de', nome, 'se chama Rafael.');
