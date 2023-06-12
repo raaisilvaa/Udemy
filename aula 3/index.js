aa48a5903eb5bbadc5aa8bb1f41ae7e53441c354
Raí Silva <121699235+raaisilvaa@users.noreply.github.com>
2023-06-11 21:31:30 -0300

    Initial commit

---
+++ aula 3/index.js
@@ -0,0 +1,7 @@
+console.log('Raí Silva'); //String = Texto
+console.log("Raí Silva");
+
+console.log(123456, 15.85); //Number = Número
+
+console.log('Meu nome é "Raí Silva" e comprei o curso hoje por volta das', 16, 'horas da tarde.')
+                                                                                                                                                                                                                                  
---
+++ aula 4/index.js
@@ -0,0 +1,8 @@
+// Escrever um comentário
+console.log('Olá mundo'); // Aqui tem outro comentário
+console.log('Linha 1'); //Comentário
+
+/*
+Um bloco de código para várias linhas
+Linha comentário
+*/
