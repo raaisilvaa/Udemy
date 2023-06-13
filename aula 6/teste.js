b2095aac6222d4bce545bf4c4088bacf0c037c63
Raí Silva <121699235+raaisilvaa@users.noreply.github.com>
2023-06-12 21:56:15 -0300

    .

---
+++ aula 6/teste.js
@@ -0,0 +1,17 @@
+let nome; //Apenas declarei a variável
+nome = 'João'; // Inicializando a variável
+console.log(nome);
+nome = 'Samuel';
+console.log(nome);
+
+REGRAS
+//Não podemos criar variáveis com palavras reservadas;
+//Variáveis precisam ter nomes significativos;
+let nomeCliente = 'João';
+console.log(nomeCliente);
+
+//Não pode começar o nome de uma variável com um número;
+//Não podem conter espaços ou traços;
+//Utilizamos camelCase;
+let nomeCompletoDoCliente = 'Raí'
+console.log(omeCompletoDoCliente);
