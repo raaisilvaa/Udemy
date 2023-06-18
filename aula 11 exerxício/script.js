0e4943bb39128313364a0f8b1b650c21285027fb
Raí Silva <121699235+raaisilvaa@users.noreply.github.com>
2023-06-18 14:34:04 -0300

    .

--- aula 11 exerxício/script.js
+++ aula 11 exerxício/script.js
@@ -2,4 +2,4 @@ let num1 = prompt('Digite um número');
 let num2 = prompt('Digite outro número');
 num1 = Number(num1);
 num2 = Number(num2);
-alert(`O resultado da sua conta foi: ${num1 + num2}!`);
+alert(`O resultado entre as duas somas foi de ${num1 + num2}`);
