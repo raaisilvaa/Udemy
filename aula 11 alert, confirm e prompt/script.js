cc4ad5143447992265faecbbb33e6599b52cce3e
Raí Silva <121699235+raaisilvaa@users.noreply.github.com>
2023-06-20 21:33:30 -0300

    commit

--- aula 11 alert, confirm e prompt/script.js
+++ aula 11 alert, confirm e prompt/script.js
@@ -1,3 +1,4 @@
+// Transformando uma string em Number.
 let num1 = prompt('Digite um número');
 let num2 = prompt('Digite outro número');
 num1 = Number(num1);
--- aula 13 mais sobre strings/index.js
+++ aula 13 mais sobre strings/index.js
@@ -1,7 +1,7 @@
 // Strings são indexiváveis, 01234567
 let umaStringg = "Um \"texto\""; // caso precisar colocar aspas no texto dentro de uma string.
 
-let umaString = "Um texto";
+let umaString = "Um rato roeu.";
 
 console.log(umaString[5]); //selecionar qual posição está meu elemento.
 
@@ -21,10 +21,22 @@ console.log(umaString.search(/x/)) // ele retorna aonde o índice foi encontrado
 
 console.log(umaString.replace('Um', 'Outro')) // substituição de caracteres.
 
-console.log(umaString.replace('Um', 'Outro')) // substituição de caracteres.
+console.log(umaString.replace(/r/g, '#')) // substituição de caracteres, se eu coloco G ele seleciona para todos com #.
 
+console.log(umaString.length); // mostra quantos caracteres tem minha string.
 
+console.log(umaString.slice(1, 6)); // selecionando de qual número quero começar e terminar minha string.
 
+console.log(umaString.slice(-3)); // começando do final, subtraindo a string.
 
+console.log(umaString.slice(-5, -1)); // começando do final, menos - o que quero.
+
+console.log(umaString.slice(1, 6));
+
+console.log(umaString.split(' r '));
+
+console.log(umaString.toLocaleUpperCase()); // deixar tudo maiúsculo.
+
+console.log(umaString.toLocaleLowerCase()); // deixar tudo minúsculo.
 
 
