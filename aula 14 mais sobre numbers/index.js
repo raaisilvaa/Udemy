12ba73119c9d987f0c0b39d8e34d1dfb6b7990e3
Raí Silva <121699235+raaisilvaa@users.noreply.github.com>
2023-06-24 23:26:03 -0300

    commit

---
+++ aula 14 mais sobre numbers/index.js
@@ -0,0 +1,7 @@
+let num1 = 10.578944444496; // number
+let num2 = 2.5; // number
+
+//console.log(num1.toString() + num2); // alterando para concatenar meu number junto com outro number.
+//num1 = num1.toString(); // alterando number pra string.
+//console.log(num1.toString(2)); // forma binária.
+console.log(num1.toFixed(2));
