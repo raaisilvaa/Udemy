7d76584dd2d69eb4e9be08153f56549b58e19644
Raí Silva <121699235+raaisilvaa@users.noreply.github.com>
2023-09-09 13:54:16 -0300

    ,

--- modelo/main.js
+++ modelo/main.js
@@ -1,15 +1,14 @@
 // Capturar evento de submit do formulário
 const form = document.querySelector('#formulario');
 
-form.addEventListener('submit', function (e) {
+form.addEventListener('submit', function(e) {
   e.preventDefault();
- 	console.log('Evento prevenido');
   setResultado('Olá mundo!');
 });
 
 function setResultado (msg) {
   const resultado = document.querySelector('#resultado');
- 	resultado.innerHTML = ''; // `<p>${msg}</p>`
+  resultado.innerHTML = '';
   const p = document.createElement('p');
   p.classList.add('paragrafo-resultado');
   p.innerHTML = 'Qualquer coisa';
