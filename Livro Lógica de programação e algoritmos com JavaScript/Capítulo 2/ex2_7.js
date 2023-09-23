0cfa0a88afc613e4b5a2392523d1b8ce0ea7ac6f
Raí Silva <121699235+raaisilvaa@users.noreply.github.com>
2023-09-23 10:17:56 -0300

    .

--- Livro Lógica de programação e algoritmos com JavaScript/Capítulo 2/ex2_7.js
+++ Livro Lógica de programação e algoritmos com JavaScript/Capítulo 2/ex2_7.js
@@ -2,7 +2,7 @@ const form = document.querySelector('#formulario');
 const resp1 = document.querySelector('h3');
 const resp2 = document.querySelector('h4');
 
-form.addEventListener("click", function(e) {
+form.addEventListener("submit", function(e) {
     e.preventDefault();
 
     const produto = form.produto.value;
@@ -13,5 +13,5 @@ form.addEventListener("click", function(e) {
     const promocao = precoMultiplicado - precoUnitario;
     
     resp1.innerText = `${produto} - Promoção: Leve 3 por R$: ${promocao.toFixed(2)}`;
-    resp2.innerHTML = `O 3 produto custa apenas R$: ${precoUnitario.toFixed(2)}`
+    resp2.innerHTML = `O 3 produto custa apenas R$: ${precoUnitario.toFixed(2)}`;
 })
