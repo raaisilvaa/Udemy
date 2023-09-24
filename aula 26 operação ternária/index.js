b4a9f504b054a95b6acc29646380f9ce1a9815f6
Raí Silva <121699235+raaisilvaa@users.noreply.github.com>
2023-09-24 19:28:18 -0300

    ...

--- aula 26 operação ternária/index.js
+++ aula 26 operação ternária/index.js
@@ -1,8 +1,8 @@
 // (condicao) ? :
-const pontuacaoUsuario = 9000;
+/*const pontuacaoUsuario = 9000;
 const nivelUsuario = (pontuacaoUsuario) >= 1000 ? 'Usuário VIP!' : 'Usuário normal';
 console.log(nivelUsuario);
-
+*/
 /*
 if (pontuacaoUsuario >= 1000) {
     console.log('Usuário VIP!');
@@ -10,3 +10,9 @@ if (pontuacaoUsuario >= 1000) {
     console.log('Usuário normal');
 }
 */
+
+const idade = 17;
+
+const entrada = (idade) >= 18 ? 'Pode entrar!' : 'Entrada recusada!';
+
+console.log(entrada);
